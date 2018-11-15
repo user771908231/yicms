4cafcd3870e18091f0f86994a6f6bf05df19968d
wm <771908231@qq.com>
2018-11-15 17:46:01 +0800

    恢复

---
+++ app/Models/AdminAttribute.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: company_windows_locahost_wm
+ * Date: 2018/11/7
+ * Time: 13:22
+ */
+
+namespace App\Models;
+
+
+use App\Models\Access\AccessControl;
+use App\Models\Open\OpenRecordOld;
+use App\Models\Open\OpenRecord;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminAttribute extends Model
+{
+    protected $table='admin_attribute';
+
+    protected $primaryKey="id";
+
+    protected $fillable=['admin_id','pid','ac_id','stop_up'];
+
+//    protected $with=['user','accessControl'];
+
+    public function user()
+    {
+        return $this->hasMany(Users::class,'companyID','ac_id');
+    }
+
+    public function openRecord()
+    {
+        sleep(5);
+        return $this->hasMany(OpenRecord::class,'ac_id','ac_id');
+    }
+
+    public function openRecordOld()
+    {
+        sleep(5);
+        return $this->hasMany(OpenRecordOld::class,'ac_id','ac_id');
+    }
+
+    public function accessControl()
+    {
+        return $this->hasOne(AccessControl::class,'ac_id','ac_id');
+    }
+}
---
+++ app/Repositories/AdminsRepository.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * YICMS
+ * ============================================================================
+ * 版权所有 2014-2017 YICMS，并保留所有权利。
+ * 网站地址: http://www.yicms.vip
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！
+ * 不允许对程序代码以任何形式任何目的的再发布。
+ * ============================================================================
+ * Created by PhpStorm.
+ * Author: kenuo
+ * Date: 2017/11/13
+ * Time: 上午9:54
+ */
+namespace App\Repositories;
+
+use App\Models\Admin;
+use Illuminate\Support\Facades\Auth;
+
+class AdminsRepository
+{
+
+    protected  $admin;
+    public function __construct(Admin $admin)
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * 创建管理员
+     * @param array $params
+     * @return mixed
+     */
+    public function create(array $params)
+    {
+        return Admin::create($params);
+    }
+    public function creates(array $params)
+    {
+        return $this->admin->creates($params);
+    }
+
+    /**
+     * 根据id获取管理员资料
+     * @param $id
+     * @return mixed
+     */
+    public function ById($id)
+    {
+        return Admin::find($id);
+    }
+
+    /**
+     * 获取管理员列表 with ('roles')
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAdminsWithRoles()
+    {
+        return Admin::with('roles')->latest('updated_at')->paginate('10');
+    }
+
+    public function thisAdminsWithRoles()
+    {
+        return Admin::where('pid',Auth::id())->with('roles')->latest('updated_at')->paginate('10');
+    }
+
+    /**
+     * 根据name查询管理员资料
+     * @param $name
+     * @return mixed
+     */
+    public function ByName($name)
+    {
+        return Admin::where('name',$name)->first();
+    }
+}
