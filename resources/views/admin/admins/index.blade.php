844fccbac6b03802675e7764bb09cf87800a2ed0
wm <771908231@qq.com>
2018-11-19 10:37:48 +0800

    dff

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -36,7 +36,6 @@
                                 @endforeach
                             </td>
                             <td class="text-center">
-                                {{dd($item->attribute)}}
                                 @switch($item->attribute->stop_up)
                                     @case(0)
                                     子管理
